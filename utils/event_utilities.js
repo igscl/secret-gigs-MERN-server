70fba4a9da43e6d94a5115e5257258db889624cb
Ignacio Giadach <igiadach@gmail.com>
2020-07-28 23:23:13 +1000

    error handling twilio outgoing sms

--- utils/event_utilities.js
+++ utils/event_utilities.js
@@ -9,7 +9,7 @@ require('dotenv').config();
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
-const {registerHelper} = require("../controllers/auth_controller")
+const { registerHelper } = require("../controllers/auth_controller")
 
 
 const getAllEvents = function (req) {
@@ -38,7 +38,7 @@ const updateApplyToEvent = async (req) => {
     let event = await Event.findById(req.params.id)
     console.log("EVENT!", event)
     let user = await User.find({
-        "username":`${req.user.username}`
+        "username": `${req.user.username}`
     })
 
 
@@ -62,8 +62,8 @@ const updateApplyToEvent = async (req) => {
         // refactor
         // if (user[0] !== undefined){
         //saves the event to the user
-            user[0].eventsApplied.push(newEventAppliedTo)
-            user[0].save()
+        user[0].eventsApplied.push(newEventAppliedTo)
+        user[0].save()
         // }
 
         return Event.findByIdAndUpdate(req.params.id, event, {
@@ -88,7 +88,7 @@ const chooseRandomUsers = async (req) => {
     let acceptedUsers = []
 
     let limit = event.capacity,
-        amount = 1,
+        amount = 2,
         lowerBound = 0,
         upperBound = event.applicants.length,
         uniqueRandomIndex = []
@@ -103,25 +103,25 @@ const chooseRandomUsers = async (req) => {
             acceptedUsers.push(randomUser)
             uniqueRandomIndex.push(index)
 
-            
+
         }
     }
-    for (let i=0 ; i<event.applicants.length; i++){
-        if (uniqueRandomIndex.includes(i)){
-            if (event.applicants[i].accepted === false){
-        event.applicants[i].accepted = true
-        let token = await Token.findOne({lives:5})
+    for (let i = 0; i < event.applicants.length; i++) {
+        if (uniqueRandomIndex.includes(i) && event.applicants[i].accepted === false) {
+            console.log(event.applicants[i].accepted)
+            event.applicants[i].accepted = true
+            let token = await Token.findOne({ lives: 5 })
 
-        
-        client.messages
-        .create({
-            body: `Good news! You've been accepted! Text the token to up to 5 friends for them to get accepted too! \n \n ${token.id}`,
-            from: '+61488839216',
-            to: `${event.applicants[i].phoneNumber}`
-        })
-        .then(message => console.log(message.sid))
-
-    }}
+            console.log("SENDING MESSAGE to:", event.applicants[i].phoneNumber)
+            client.messages
+            .create({
+                body: `Good news! You've been accepted! Text the token to up to 5 friends for them to get accepted too! \n \n ${token.id}`,
+                from: '+61488839216',
+                to: `${event.applicants[i].phoneNumber}`
+            })
+            .then(message => console.log(message.sid))
+            .catch(e => { console.error('Got an error:', e.code, e.message); });
+        }
     }
 
     return Event.findByIdAndUpdate(req.params.id, event, {
@@ -130,36 +130,37 @@ const chooseRandomUsers = async (req) => {
 
 }
 
-const findAndAcceptTokenUser = async (req) =>{
+const findAndAcceptTokenUser = async (req) => {
     //if phone number has applied to event, accept it
     // if req.body.From is in Event
-    try{
-    let event = await Event.find(
-        {"applicants.phoneNumber": req.body.From})
-        
-        console.log("FOUND MATCHES!",event[0])
+    try {
+        let event = await Event.find(
+            { "applicants.phoneNumber": req.body.From })
+
+        console.log("FOUND MATCHES!", event[0])
         indexMatch = event[0].applicants.findIndex(x => x.phoneNumber === `${req.body.From}`)
         console.log(indexMatch)
         event[0].applicants[indexMatch].accepted = true
         event[0].save()
-        console.log("SECOND TIME",event[0])
+        console.log("SECOND TIME", event[0])
 
 
         return Event.findByIdAndUpdate(event[0].id, event[0], {
             new: true
         })
-    
-    }catch(err){
-            try{
-                // if cannot find user on DB then create a user with the phone number
-                let user = await User.find({
-                    "phoneNumber":`${req.body.From}`})
-                    console.log(user)
-                if (user[0] === undefined){
-                    registerHelper(req)
-                    console.log("sending the MESSAGE")
 
-                    client.messages
+    } catch (err) {
+        try {
+            // if cannot find user on DB then create a user with the phone number
+            let user = await User.find({
+                "phoneNumber": `${req.body.From}`
+            })
+            console.log(user)
+            if (user[0] === undefined) {
+                registerHelper(req)
+                console.log("sending the MESSAGE")
+
+                client.messages
                     .create({
                         body: `We couldn't find your number, so we created a user. Log into the website with username: ${req.body.From.substr(1)} and password: temporary`,
                         from: '+61488839216',
@@ -167,20 +168,21 @@ const findAndAcceptTokenUser = async (req) =>{
                     })
                     .then(message => console.log(message.sid))
 
-                }
-            }catch(err){
-                
             }
-    
+        } catch (err) {
+
+        }
+
     }
 }
 
 const findMyEvents = async (eventArray) => {
-    console.log("findMyEvents!:",eventArray)
+    console.log("findMyEvents!:", eventArray)
     const queryArray = eventArray.map((eventObject) => mongoose.Types.ObjectId(eventObject.eventId))
 
     return Event.find({
-        '_id': { $in: queryArray}})
+        '_id': { $in: queryArray }
+    })
 
     // console.log(queryArray)
     // return queryArray
